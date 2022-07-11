db540dace08eecb686eeebabac803b1540581f55
DS407 <karmalet@duksung.ac.kr>
2022-07-11 21:39:25 +0900

    add execute file

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@
 
 import thulac
 
-thu1 = thulac.thulac(seg_only=True, model_path="请查看README下载相关模型放到thulac根目录或在这里写路径")  #设置模式为行分词模式
+thu1 = thulac.thulac(seg_only=False, model_path="./models", deli='/')  #设置模式为行分词模式
 a = thu1.cut("我爱北京天安门")
 
 print(a)
